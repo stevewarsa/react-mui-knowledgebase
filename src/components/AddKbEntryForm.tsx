12f7ea66fa68fcde4428d2c03c14dcc23006f348
Steve Warsa <swarsa480@gmail.com>
2021-11-28 05:51:02 -0700

    Changed to using an iconbutton with chevrons to show/hide add form - but had to use event.preventDefault to stop it from registering 2 button clicks - which I didn't really like

--- src/components/AddKbEntryForm.tsx
+++ src/components/AddKbEntryForm.tsx
@@ -1,12 +1,14 @@
-import {Box, Button, Collapse, Container, FormControlLabel, Stack, Switch, TextField} from "@mui/material";
+import {Box, Button, Collapse, Container, FormControlLabel, IconButton, Stack, Switch, TextField} from "@mui/material";
 import {useState} from "react";
 import SaveIcon from '@mui/icons-material/Save';
-import {Cancel} from "@mui/icons-material";
+import {Cancel, ChevronRight, ExpandMore} from "@mui/icons-material";
 
 const AddKbEntryForm = () => {
     const [showForm, setShowForm] = useState(false);
-    const handleToggleForm = () => {
+    const handleToggleForm = (event) => {
+        console.log("handleToggleForm");
         setShowForm(prevState => !prevState);
+        event.preventDefault();
     };
 
     const handleAddEntry = () => {
@@ -20,8 +22,14 @@ const AddKbEntryForm = () => {
     return (
         <Container>
             <FormControlLabel
-                control={<Switch checked={showForm} onChange={handleToggleForm}/>}
                 label={showForm ? "Hide Add Form" : "Show Add Form"}
+                control={
+                    <IconButton
+                        onClick={handleToggleForm}
+                        sx={{m: 2}}>
+                        {showForm ? <ExpandMore sx={{fontSize: 40}} /> : <ChevronRight sx={{fontSize: 40}}/>}
+                    </IconButton>
+                }
             />
             <Collapse in={showForm}>
                 <Stack spacing={2} sx={{p: 5}}>
@@ -29,8 +37,10 @@ const AddKbEntryForm = () => {
                     <TextField label="Description" multiline minRows={3} variant="outlined"/>
                     <TextField label="New/Existing Tag" variant="outlined"/>
                     <Box>
-                        <Button startIcon={<SaveIcon/>} onClick={handleAddEntry} variant="contained">Add Entry</Button>
-                        <Button startIcon={<Cancel/>} onClick={handleCancel} color="secondary">Cancel</Button>
+                        <Button startIcon={<SaveIcon/>} onClick={handleAddEntry} variant="contained">Add
+                            Entry</Button>
+                        <Button startIcon={<Cancel/>} onClick={handleCancel}
+                                color="secondary">Cancel</Button>
                     </Box>
                 </Stack>
             </Collapse>
