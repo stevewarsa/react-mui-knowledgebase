93f9ef1443bf1b0696859ab73454763b473085d6
Steve Warsa <swarsa480@gmail.com>
2021-12-04 08:14:26 -0700

    Added new package react-markdown and sample component that shows basic usage

---
+++ src/components/MarkdownToHtml.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+export default function MarkdownToHtml(){
+    return(
+        <ReactMarkdown>*React-Markdown* is **Awesome**</ReactMarkdown>
+    )
+}
