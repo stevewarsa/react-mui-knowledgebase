9a10ef94505e6e298cfaf12cc1afad620dbbf4e6
Steve Warsa <swarsa480@gmail.com>
2023-10-17 05:52:49 -0700

    Fixed Mixed Content warning and also error:0308010C:digital envelope routines::unsupported

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
     app.use(
         createProxyMiddleware('/kb', {
-            target: 'http://localhost:8080',
+            target: 'http://127.0.0.1:8080',
             changeOrigin: true,
             logLevel: "debug",
             headers: {
